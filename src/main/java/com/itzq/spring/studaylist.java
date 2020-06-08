e01e94e1471bf65b66ccf713782a60f6b046e589
wangzq <13123>
2020-06-08 22:08:23 +0800

    集合

--- src/main/java/com/itzq/spring/studaylist.java
+++ src/main/java/com/itzq/spring/studaylist.java
@@ -1,15 +1,22 @@
 package com.itzq.spring;
 
-import java.util.List;
-import java.util.UUID;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class studaylist {
+
     public static void main(String[] args) {
-        List list= new CopyOnWriteArrayList();//Collections.synchronizedList(new ArrayList<>());//new Vector();//new ArrayList();
+        //List list= new CopyOnWriteArrayList();//Collections.synchronizedList(new ArrayList<>());//new Vector();//new ArrayList();
+        //Set list= new CopyOnWriteArraySet();
+        new HashMap<>();
+        Map<String , String> list= new ConcurrentHashMap<>();//new HashMap<>();
         for (int i = 0; i <30; i++) {
             new Thread(()->{
-                list.add(UUID.randomUUID().toString().substring(0,5));
+                list.put(Thread.currentThread().getName(),UUID.randomUUID().toString().substring(0,5));
                 System.out.println(list);
             },String.valueOf(i)).start();
         }
