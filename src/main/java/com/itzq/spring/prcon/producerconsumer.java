859dba2bb3b0cb7aa93d26171cc3f659b7ea3b76
wangzq <18342153080@163.COM>
2020-06-10 15:11:25 +0800

    steam

---
+++ src/main/java/com/itzq/spring/prcon/producerconsumer.java
@@ -0,0 +1,104 @@
+package com.itzq.spring.prcon;
+
+import java.util.PrimitiveIterator;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author wangzq
+ * @create 2020-06-10 14:25
+ */
+class resource {
+    private Integer number=0;
+    public synchronized  void reduce(){
+        while (number==0){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        number--;
+        System.out.println(Thread.currentThread().getName()+"==来消费了=="+number);
+        this.notifyAll();
+    }
+    public synchronized  void increase(){
+        while (number!=0){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        number++;
+        System.out.println(Thread.currentThread().getName()+"==来生产了=="+number);
+        this.notifyAll();
+    }
+}
+class resource2 {
+    private Integer number=0;
+    Lock lock=new ReentrantLock();
+    Condition condition = lock.newCondition();
+    public void reduce(){
+        lock.lock();
+        try {
+            while (number==0){
+                //this.wait();
+                condition.await();
+            }
+        number--;
+        System.out.println(Thread.currentThread().getName()+"==新来消费了=="+number);
+        //this.notifyAll();
+        condition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+
+    }
+    public void increase(){
+        lock.lock();
+        try {
+            while (number!=0){
+                //this.wait();
+                condition.await();
+            }
+            number++;
+            System.out.println(Thread.currentThread().getName()+"==新来生产了=="+number);
+            //this.notifyAll();
+            condition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+
+    }
+}
+public class producerconsumer {
+    public static void main(String[] args) {
+        resource2 resource=new resource2();
+        new Thread(()->{
+            for (int i = 0; i < 30; i++) {
+                resource.reduce();
+            }
+        },"AAA").start();
+        new Thread(()->{
+            for (int i = 0; i < 30; i++) {
+                resource.increase();
+            }
+        },"BBB").start();
+        new Thread(()->{
+            for (int i = 0; i < 30; i++) {
+                resource.reduce();
+            }
+        },"CCC").start();
+        new Thread(()->{
+            for (int i = 0; i < 30; i++) {
+                resource.increase();
+            }
+        },"DDD").start();
+    }
+}
